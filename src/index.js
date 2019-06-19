42e5fb219b1a3c91fc02f1a0aa4f9d6d4ef26a85
Alireza Molaee <alirezamolaii@gmail.com>
2019-06-19 15:24:19 +0430

    add article page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import moment from "moment-jalaali";
+import fa from "moment/locale/fa";
+
+moment.locale("fa", fa);
+moment.loadPersian({usePersianDigits: true});
 
 library.add(fas)
 
